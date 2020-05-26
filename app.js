27b4e9e7425e1b05dcaff57289fd445f20de5e0d
thiefunny <mikolaj@walanus.com>
2020-05-26 13:05:07 +0200

    paseczki

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const parentEl = document.querySelector(".parent");
-const divEl = document.querySelector("div");
-const buttonEl = document.querySelector("button");
 
-
-for (let i = 0; i < parentEl.childElementCount + 1; i++) {
-
-parentEl[i].style.width = (i+1)*20 +"px";
-parentEl[i].style.height = "100px";
+for (let i = 0; i < parentEl.childElementCount; i++) {
+    
+parentEl.children[i].style.width = (i + 1) * 10 +"px";
+parentEl.children[i].style.height = "10px";
+parentEl.children[i].style.backgroundColor = "rgb("+(200-i*i)+","+i*i/2+","+i*10+")";
+// console.log(parentEl.children[i].getAttribute("style"));
 
 }
