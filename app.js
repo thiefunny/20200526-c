93f00414baac9b03ff087d8a4a4aacafb06c0275
thiefunny <mikolaj@walanus.com>
2020-05-26 13:35:34 +0200

    co drugi

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const parentEl = document.querySelector(".parent");
 
 for (let i = 0; i < parentEl.childElementCount; i++) {
-    
-parentEl.children[i].style.width = (i + 1) * 10 +"px";
-parentEl.children[i].style.height = "10px";
-parentEl.children[i].style.backgroundColor = "rgb("+(200-i*i)+","+i*i/2+","+i*10+")";
-// console.log(parentEl.children[i].getAttribute("style"));
 
+    parentEl.children[i].style.width = (i + 1) * 10 + "px";
+    parentEl.children[i].style.height = "10px";
+    parentEl.children[i].style.backgroundColor = "rgb(" + (200 - i * i) + "," + i * i / 2 + "," + i * 10 + ")";
+
+    if (i % 2 === 1) {
+        parentEl.children[i].style.backgroundColor = "rgb(" + (i * 10) + "," + i * 20 + "," + i * 10 + ")";
+    }
 }
